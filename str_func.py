dc713aeb237929a90a8f74f36426461b63f36ef7
User <zasbox@gmail.com>
2023-09-16 16:00:21 +0500

    add to uppercase first symbol function

--- str_func.py
+++ str_func.py
@@ -1,2 +1,9 @@
 def to_upper_case(string):
+    """Возвращает строку, состоящую из заглавных букв"""
     return string.upper()
+
+
+def to_upper_case_first_symbols(string):
+    """Возвращает строку из слов, начинающихся с заглавной буквы"""
+    words = str(string).split()
+    return [word[0].upper() + word[1:] for word in words]
